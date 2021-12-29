bb33bfd8abb1a0b080e6e0bdb9ea3215e13bd235
Christian Moreira <chmleave@gmail.com>
2021-12-29 19:36:59 -0300

    ignore files

--- src/servico_clojure/core.clj
+++ src/servico_clojure/core.clj
@@ -1,19 +1,3 @@
-(ns servico-clojure.core
-  (:require [io.pedestal.http.route :as route]
-            [io.pedestal.http :as http]))
+(ns servico-clojure.core)
 
-(defn funcao-hello [request]
-  {:status 200 :body (str "Hello World "
-                          (get-in request [:query-params :name] "Amigui")
-                          "!")})
-
-(def routes (route/expand-routes
-             #{["/hello" :get funcao-hello :route-name :hello-world]}))
-
-(def service-map {::http/routes routes
-                  ::http/port 9999
-                  ::http/type :jetty
-                  ::http/join? false})
-
-(http/start (http/create-server service-map))
-(println "Started server http")
+(println "Started the app")
